a611ddeafb491d68be89e021d3fd1347e37eae50
rfrias101 <friasprinze@gmail.com>
2025-05-01 10:35:16 +0800

    UI Functionality for start
    Bleh

---
+++ Kwentoon Game Jam 2025/Assets/Script/SceneManagement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManagement : MonoBehaviour
+{
+    public string SceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
