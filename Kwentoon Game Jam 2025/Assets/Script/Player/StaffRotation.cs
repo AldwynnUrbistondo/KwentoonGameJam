9f90c68d3c8a42ad06254a40b97b9c7c23ec6186
AldwynnUrbistondo <aldwynn020505@gmail.com>
2025-05-02 22:46:30 +0800

    .

--- Kwentoon Game Jam 2025/Assets/Script/Player/StaffRotation.cs
+++ Kwentoon Game Jam 2025/Assets/Script/Player/StaffRotation.cs
@@ -13,12 +13,16 @@ public class StaffRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
+        if (!GameManager.isPause)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f;
 
-        Vector3 direction = mousePos - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Vector3 direction = mousePos - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+            transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+        }
+        
     }
 }
